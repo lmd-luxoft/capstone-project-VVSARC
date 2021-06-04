7ff402d6b09554e08e8d3ed7d7a73d37af5d547d
Владимир Смирнов <vvsarc@gmail.com>
2021-06-04 19:51:44 +0400

    2

---
+++ HomeAccounting.CompositionRoot/AspNetApplicationBuilder.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeAccounting.CompositionRoot
+{
+    public class AspNetApplicationBuilder : AbstractApplicationBuilder
+    {
+        public AspNetApplicationBuilder(IServiceCollection services) : base (services)
+        {
+
+
+        }
+
+        protected override void RegisterBuisenessLogic()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void RegisterDataSource()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void RegisterInfrastructure()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
